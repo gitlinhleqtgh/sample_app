b1ce0611e495832cc33e808bd5d316ce3fc3f7ab
gitlinhleqtgh <linhleqtgh@gmail.com>
2021-02-25 08:23:42 +0700

    chapter 6
    add setting
    
    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :name, presence: true
+  validates :email, presence: true,
+    length: {maximum: Settings.size},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: true
+
+  before_save :downcase_email
+
+  has_secure_password
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
